6b09683e6fff30128478f3c0ba4c75bdd5c9a722
MaSsoumeh <massoumehzarei@gmail.com>
2021-11-04 20:48:14 +0330

    Create NewMeetup Page

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,4 +1,12 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import selector from "./NewMeetup.module.css";
+
 function NewMeetupPage() {
-  return <div>New Meetup Page</div>;
+  return (
+    <section className={selector.section}>
+      <h2>Add New Meetup </h2>
+      <NewMeetupForm />
+    </section>
+  );
 }
 export default NewMeetupPage;
